c8275bfd8a884c31040e1f6e61ed6ca58de796d5
Crowdbotics <team@crowdbotics.com>
2020-04-10 03:24:31 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/potomac_pest_15699/MainActivity.java
@@ -0,0 +1,15 @@
+package com.potomac_pest_15699;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "potomac_pest_15699";
+    }
+}
